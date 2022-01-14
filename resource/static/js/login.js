51a7a0cafd77afb7742c735c579379f62d0f939c
YoungJinGo <skyyj32@naver.com>
2022-01-15 07:10:15 +0900

    ✅ login js 파일 생성

---
+++ resource/static/js/login.js
@@ -0,0 +1,8 @@
+document.getElementById('signup-check-btn').addEventListener('click', signUpCheck);
+
+
+function signUpCheck(){
+    console.log('hello');
+    return location.href = "http://localhost:8080/login";
+}
+
