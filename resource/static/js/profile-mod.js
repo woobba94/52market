b6e1853ecae09b4121347824d9140011918a3a84
unimeang <aodgkfud4@gmail.com>
2022-01-20 15:35:34 +0900

    ✨ Add: 프로필 정보 수정 기능 구현(id 중복 검사 제외)

--- resource/static/js/profile-mod.js
+++ resource/static/js/profile-mod.js
@@ -1,32 +1,18 @@
-// 1. 본인의 프로필 정보를 받아오기 ok
-// 2. 받은 정보 뿌리기
-// 3. 이미지 수정 (1개만 선택 가능) ok
-// 4. input 값 입력받아서 출력
-//      사용자 id 중복, (유효성) 검사 
-//      if == 유효하다  => 데이터 제출 (사진, id, 이름, intro)
-//      else warning msg Show 
-// 5. 저장 클릭 이벤트 
-//      데이터 서버전송 ?? 수정 
-//      마이 프로필 페이지로 이동    
-
 
 //localStorage
 // const token = localStorage.getItem('token');
 // const userId = localStorage.getItem('accountname');
 
-//GET해서 뿌려주는게 PUT으로만 수정하면되나?
 
-let url = "http://146.56.183.55:5050"; 
-// const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTdjZGVhNDU4ZjFkZGQyZTI3MzE2OSIsImV4cCI6MTY0Nzc2NTYwMSwiaWF0IjoxNjQyNTgxNjAxfQ.OqRUavCu5WNb8CWe1ZLLmqChD9n2uMZ3BSH-La2PuTc';
+const urls = "http://146.56.183.55:5050"; 
 const $imageUpload = document.querySelector('chooseImg');
 const $imagePre = document.querySelector('.imgPre');
 const hrefLink = location.href;
-const saveBtn = document.querySelector('.submit-btn'); 
+const $saveBtn = document.querySelector('.submit-btn'); 
 
-
-
+// user 정보 받아서 뿌려주기
 async function getUserData() {
-    const res = await fetch(`${url}/profile/aodgkfud`, {
+    const res = await fetch(`${urls}/profile/이아무가누구냐`, {
         method:"GET",
         headers: {
             "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTdjZGVhNDU4ZjFkZGQyZTI3MzE2OSIsImV4cCI6MTY0Nzc2NTYwMSwiaWF0IjoxNjQyNTgxNjAxfQ.OqRUavCu5WNb8CWe1ZLLmqChD9n2uMZ3BSH-La2PuTc",
@@ -50,7 +36,7 @@ getUserData();
 async function imageUpload(files) {
     const formData = new FormData();
     formData.append("image", files[0]);
-    const res = await fetch(`${url}/image/uploadfile`, {
+    const res = await fetch(`${urls}/image/uploadfile`, {
     method: 'POST',
     body: formData
     })
@@ -73,57 +59,41 @@ document.querySelector('#chooseImg').addEventListener("change", profileImage);
 // uploadProfile.src = userProfile;
 // uploadProfile.setAttribute('alt', `${userId}님의 프로필`);
 
-// const res = await fetch (`${url}/profile/hey_binky`, {
-    //     method: "PUT",
-    //     headers : {
-        //         "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxY2E2MzhhYjVjNmNkMTgwODRlNDQ3ZCIsImV4cCI6MTY0NzcwMjM1MSwiaWF0IjoxNjQyNTE4MzUxfQ.SwqCtFFct_LKNGNcdUbRAlRT4AvC9KqJmg76T1qKOPo",
-        //         "Content-Type" : "application/json"
-        //     },
-        //     body : JSON.stringify({
-            //         "user": {
-                //             "username": String,
-                //             "accountname": String,
-                //             "intro": String,
-                //             "image": String
-                //         }
-                //     })
-                // })
-                
                 //Id 중복 검사
-                async function checkIdValid(id) {
-                    const res = await fetch(`${url}/user/`+'accountnamevalid', {
-                        method:"POST",
-                        headers: {
-                            "Content-Type": "application/json",
-                        },
-                        body:JSON.stringify({
-                            "user":{
-                                "accountname": String
-                            }
-                        })
-                    })
-                    const json = await res.json()
-                    return json.message == "사용 가능한 아이디 입니다." ? true : false
-                }
+                // async function checkIdValid(id) {
+                //     const res = await fetch(`${url}/user/accountnamevalid, {
+                //         method:"POST",
+                //         headers: {
+                //             "Content-Type": "application/json",
+                //         },
+                //         body:JSON.stringify({
+                //             "user":{
+                //                 "accountname": String
+                //             }
+                //         })
+                //     })
+                //     const json = await res.json()
+                //     return json.message == "사용 가능한 아이디 입니다." ? true : false
+                // }
                 
                 
                 
-                document.querySelector(".submit-btn").addEventListener('click', async () => {
-                    const id = document.querySelector("#user-id").value
-                    const idValid = await checkIdValid(id)
-                    const failMsg = document.querySelector('.input-warning-msg');
+                // document.querySelector(".submit-btn").addEventListener('click', async () => {
+                //     const id = document.querySelector("#user-id").value
+                //     const idValid = await checkIdValid(id)
+                //     const failMsg = document.querySelector('.input-warning-msg');
                     
-                    if (idValid) {
-                        alert ("사용 가능한 ID 입니다.")
-                    } else {
-                        failMsg.style.display = "block";
-                    }
-                })
+                //     if (idValid) {
+                //         alert ("사용 가능한 ID 입니다.")
+                //     } else {
+                //         failMsg.style.display = "block";
+                //     }
+                // })
             
             
-            //프로필 수정 input + id 중복 검사
+            //프로필 수정 input + id 중복 검사 해야함
             async function editProfile() {
-                const userName = document.querySelector('#user-id').value;
+                const userName = document.querySelector('#user-name').value;
                 const userId = document.querySelector('#user-id').value;
                 const intro = document.querySelector('#intro-input').value;
                 const imageUrl = document.querySelector('.imgPre').src 
@@ -145,55 +115,21 @@ document.querySelector('#chooseImg').addEventListener("change", profileImage);
                     })
 
                 console.log(res)
-                const json = await res.json()
+                const json = await res.json();
                 console.log(json, "수정된 값 찍어줘");
                 // // const failMsg = "이미 쓰고있음"
                 // const message = json.message;
                 // if (json.message !== failMsg)
                 if (res.status == 200) {
-                    location.href = "/profile-my"
+                    location.href = "/profile-mod"
                 } else {
                     console.log(json);  
                 }
             } catch(err) {
-                alert('에러났네');
+                alert('/404');
             }
-  // input 값 
-// async function join() {
-//     const userName = document.querySelector('#user-id');
-//     const userId = document.querySelector('#user-id');
-//     const intro = document.querySelector('#intro-input');
-//     const imageUrl = document.querySelector('.imgPre').src 
-//     try {
-//         const res = await fetch("http://146.56.183.55:5050/user", {
-//             method : "PUT",
-//             headers : {
-//                 "Content-Type" : "application/json",
-//                 "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxY2E2MzhhYjVjNmNkMTgwODRlNDQ3ZCIsImV4cCI6MTY0NzcwMjM1MSwiaWF0IjoxNjQyNTE4MzUxfQ.SwqCtFFct_LKNGNcdUbRAlRT4AvC9KqJmg76T1qKOPo" 
-//             },
-//             body : JSON.stringify({
-//                 "user" : {
-//                     "username" : userName.value,
-//                     "accountname" : userId.value,
-//                     "intro" : intro.value,
-//                     "image" : imageUrl,
-//                 }
-//             })
-//         })
-//     const json = await res.json()
-//     console.log(json, "수정된 값 찍어줘");
-    
-//     const failMsg = "이미 쓰고있음"
-// ;
-//     if (json.message !== failMsg) {
-//         location.href = "/profile-my"
-//     } else {
-//         console.log(json); //3번 
-//     }
-// } catch(err) {
-//     alert('에러났네');
-// }
-// }
- //버튼 활성화 
-saveBtn.addEventListener('click', editProfile);
         }
+
+ //버튼 활성화 
+$saveBtn.addEventListener('click', editProfile)
+                
