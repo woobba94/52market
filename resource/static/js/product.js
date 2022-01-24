7b3121a93e8ce5e5785c4b92fba7fe8d0461c8c7
woobba94 <jwj3199@naver.com>
2022-01-24 18:33:49 +0900

    ✅ Update: Product edit function

--- resource/static/js/product.js
+++ resource/static/js/product.js
@@ -23,14 +23,18 @@ productUrlBox.addEventListener('blur', (event) => {
 });
 
 // 저장버튼 리스너
-saveBtn.addEventListener('click', postProductData);
+// 이미 존재하는 상품일 경우
+if (location.href.split('product')[1] != '') {
+  document.querySelector('.button-save').disabled = false;
+  saveBtn.addEventListener('click', editProductData);
+} else saveBtn.addEventListener('click', postProductData);
 
 // 첨부된 이미지 렌더링
 function postProductImg() {
   const imgInputBtn = document.querySelector('.image-input-field input');
   const productImgBox = document.querySelector('.image-input-field');
   let productImgElement = document.querySelector('.image-input-field img');
-  // console.log(productImgElement);
+
   imgInputBtn.addEventListener('change', (e) => {
     this.productImgName = '';
     // 최초 업로드일 경우 img태그 새로 만들기
@@ -59,7 +63,6 @@ function postProductImg() {
       .then((res) => res.json())
       .then((data) => {
         this.productImgName = data['filename'];
-        btnActive();
       })
       .catch((err) => console.err(err));
   });
@@ -69,8 +72,8 @@ function postProductImg() {
 async function postProductData() {
   const productName = document.querySelector('#product-name');
   const productPrice = document.querySelector('#product-price');
-  const productImgName = window.productImgName;
-  // console.log(productImgName);
+  // const productImgName = window.productImgName;
+  console.log(productImgName);
   const price = parseInt(productPrice.value.replaceAll(',', ''), 10);
   const url = `http://146.56.183.55:5050/product`;
   const response = await fetch(url, {
@@ -93,105 +96,88 @@ async function postProductData() {
   console.log(data);
   if (data) {
     // this.dataReset();
-    window.location = `http://127.0.0.1:8080/profile/${localStorage.getItem('accountname')}`;
+    window.location = `/profile/${localStorage.getItem('accountname')}`;
   }
 }
 
 // ----------------- 기존 상품 수정 로직 -----------------
 // 상품 정보 가져오기
 async function getProductData() {
-  const response = await fetch(`http://146.56.183.55:5050/product/detail/${this.productId}`, {
+  const productId = location.href.split('/product/')[1];
+  const response = await fetch(`http://146.56.183.55:5050/product/detail/${productId}`, {
     headers: {
-      Authorization: 'Bearer ' + localStorage.getItem('access-token'),
+      Authorization: 'Bearer ' + localStorage.getItem('token'),
     },
   });
   this.productData = await response.json();
 }
 
-// // 현재 상품 정보 세팅
-// async function setCurrentData() {
-//   console.log('현재 상품 정보 세팅');
-//   const productName = document.querySelector('#productNameInput');
-//   const productPrice = document.querySelector('#priceInput');
-//   const storeLink = document.querySelector('#storeLinkInput');
-//   const productImgInput = documenthis.querySelector('#productImgInput');
-//   const productImgBox = document.querySelector('#product-img-box');
+// 현재 상품 정보 세팅
+async function setCurrentData() {
+  console.log('현재 상품 정보 세팅');
+  const productName = document.querySelector('#product-name');
+  const productPrice = document.querySelector('#product-price');
+  const productUrl = document.querySelector('#product-url');
+  const productImgBox = document.querySelector('.image-input-field');
 
-//   const productImgElement = document.createElement('img');
-//   productImgBox.append(productImgElement);
+  const productImgElement = document.createElement('img');
+  productImgBox.append(productImgElement);
 
-//   await this.getProductData();
+  await this.getProductData();
 
-//   const data = this.productData['product'];
-//   this.productImgName = data['itemImage'];
-//   productImgElement.src = this.productImgName;
-//   productName.value = data['itemName'];
-//   productPrice.value = data['price'];
-//   storeLink.value = data['link'];
+  const data = this.productData['product'];
+  this.productImgName = data['itemImage'];
+  productImgElement.src = this.productImgName;
+  productName.value = data['itemName'];
+  productPrice.value = data['price'];
+  productUrl.value = data['link'];
+}
 
-//   productImgInput.setAttribute('data-state', 1);
-//   productName.setAttribute('data-state', 1);
-//   productPrice.setAttribute('data-state', 1);
-//   storeLink.setAttribute('data-state', 1);
-
-//   this.btnActive();
-// }
-
-// function href(pageName) {
-//   const routeTag = document.createElement('a');
-//   routeTag.id = 'routeTag';
-//   routeTag.href = `/pages${pageName}`;
-//   document.querySelector('.container').appendChild(routeTag);
-//   document.querySelector('#routeTag').click();
-// }
-
-// // 상품 정보 서버로 전송 - 수정
-// function editProductData() {
-//   const saveBtn = document.querySelector('.button-save');
-//   saveBtn.addEventListener('click', async () => {
-//     const productName = document.querySelector('#productNameInput');
-//     const productPrice = document.querySelector('#priceInput');
-//     const storeLink = document.querySelector('#storeLinkInput');
-
-//     const price = parseInt(productPrice.value.replaceAll(',', ''), 10);
-//     const response = await fetch(`http://146.56.183.55:5050/product/${this.productId}`, {
-//       method: 'PUT',
-//       headers: {
-//         'Content-Type': 'application/json',
-//         Authorization: 'Bearer ' + localStorage.getItem('access-token'),
-//       },
-//       body: JSON.stringify({
-//         product: {
-//           itemName: productName.value,
-//           price: price,
-//           link: storeLink.value,
-//           itemImage: this.productImgName,
-//         },
-//       }),
-//     });
-
-//     const data = await response.json();
-
-//     if (data) {
-//       this.dataReset();
-//       href('/profile');
-//     }
-//   });
-// }
-
-// 상품 정보 전송 후 데이터 리셋
-function dataReset() {
-  console.log('상품 정보 전송 후 데이터 리셋vv');
-  const inputs = document.querySelectorAll('INPUT');
-  inputs.forEach((item) => {
-    item.value = '';
-
-    if (item.getAttribute('type') == 'file') {
-      const imgTag = document.querySelector('#product-img-box img');
-      imgTag.remove();
-      this.productImgName = '';
+// 상품 정보 서버로 전송 - 수정
+async function editProductData() {
+  const productName = document.querySelector('#product-name');
+  const productPrice = document.querySelector('#product-price');
+  const productUrl = document.querySelector('#product-url');
+  const productImgSrc = document.querySelector('.image-input-field img').src;
+  const price = parseInt(productPrice.value.replaceAll(',', ''), 10);
+  const response = await fetch(
+    `http://146.56.183.55:5050/product/${location.href.split('/product/')[1]}`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('token'),
+      },
+      body: JSON.stringify({
+        product: {
+          itemName: productName.value,
+          price: price,
+          link: productUrl.value,
+          itemImage: productImgSrc,
+        },
+      }),
     }
+  );
+
+  const data = await response.json();
+  console.log(data);
+  if (data) {
+    // deleteProduct(location.href.split('/product/')[1]);
+    window.location = `/profile/${localStorage.getItem('accountname')}`;
+  }
+}
+
+// 상품 삭제
+async function deleteProduct(tagetId) {
+  const response = await fetch(`http://146.56.183.55:5050/product/${tagetId}`, {
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Bearer ' + localStorage.getItem('token'),
+      'Content-Type': 'application/json',
+    },
   });
+
+  const data = await response.json();
 }
 
 // ------------------ 형식 체크 함수들 --------------------
@@ -228,12 +214,13 @@ function isFilledAll() {
 // 저장 버튼 활성화 여부
 function activeCheckSaveButton() {
   const imgBox = document.querySelector('.image-input-field img');
-  // console.log(imgBox);
   const saveBtn = document.querySelector('.button-save');
   if (isFilledAll() && isValidUrl() && imgBox) saveBtn.disabled = false;
   else saveBtn.disabled = true;
 }
 
 window.addEventListener('DOMContentLoaded', () => {
+  // 이미 존재하는 상품일 경우
+  if (location.href.split('product')[1] != '') setCurrentData();
   postProductImg();
 });
