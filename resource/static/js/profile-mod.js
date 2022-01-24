a311675d8e7899c0fce6294a47ad84e9d77a6bc0
unimeang <aodgkfud4@gmail.com>
2022-01-24 23:18:11 +0900

    ✅ : 프로필 수정기능 구현 ( ID값 변경 불가 및 사용자 이름 유효성 검사

--- resource/static/js/profile-mod.js
+++ resource/static/js/profile-mod.js
@@ -10,8 +10,14 @@ const $saveBtn = document.querySelector('.submit-btn');
 // console.log(newUserNames);
 
 const warningMsg1 = document.querySelector('.error-message1');
-const warningMsg2 = document.querySelector('.error-message2'); //사용중인 아이디
+const warningMsg2 = document.querySelector('.error-message2');
+const warningMsg3 = document.querySelector('.error-message3');
+//사용중인 아이디
+
 // user 정보 받아서 뿌려주기
+const myName = document.querySelector("#user-name");
+const myId = document.querySelector("#now-user-id");
+
 async function getUserData() {
     const res = await fetch(`${url}/profile/${myAccountName}`, {
         method: "GET",
@@ -23,12 +29,10 @@ async function getUserData() {
     const result = await res.json();
     console.log(result);
 
-    const myName = document.querySelector("#user-name");
     myName.value = result.profile.username;
-    const myId = document.querySelector("#user-id");
-    // console.log(result.profile, "프로필");
-    // console.log(result.prorile.accountname, "어카운");
+
     myId.value = result.profile.accountname;
+
     const myIntro = document.querySelector("#intro-input");
     myIntro.value = result.profile.intro;
     const myImage = document.querySelector(".imgPre");
@@ -59,58 +63,80 @@ async function profileImage(e) {
 document.querySelector('#chooseImg').addEventListener("change", profileImage);
 
 
+// let userIds = myId.value;
+// console.log(userIds);
 // 아이디 중복 검사 
-async function checkIdValid(id) {
-    const res = await fetch(`${url}/user/accountnamevalid`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            "user": {
-                "accountname": username
-            }
-        })
-    })
-    const checkId = await res.json();
-    return checkId.message == "사용 가능한 계정ID 입니다." ? true : false
-}
+// async function checkIdValid(newMyId) {
 
-// id 유효성 정규식 구문 (영문 숫자 밑줄 하이픈)
-function id_check(idVal) {
-    const userIdReg = /^[a-zA-Z0-9_.]/;
-    return userIdReg.test(idVal) === true ? true : false;
-}
+//     const res = await fetch(`${url}/user/accountnamevalid`, {
+//         method: "POST",
+//         headers: {
+//             "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify({
+//             "user": {
+//                 "accountname": newMyId,
+//             }
+//         })
+//     })
+//     const checkId = await res.json();
 
-function idError() {
-    const newUserNames = document.querySelector('#user-id');
-    const test = newUserNames.value;
-    if (id_check(test)) {
+//     return checkId.message == "사용 가능한 계정ID 입니다." ? true : false
+// }
+
+
+// user-name 글자 수 검사
+function userNameVal(userName) {
+    if (userName.length < 2) {
+        warningMsg1.style.display = 'block';
         return true;
     }
     return false;
 }
 
+// id 유효성 정규식 구문 (영문 숫자 밑줄 하이픈)
+// function id_check(idVal) {
+//     const userIdReg = /^[a-zA-Z0-9_.]/;
+//     return userIdReg.test(idVal) === true ? true : false;
+// }
+
+// function idError() {
+//     const test = myId.value;
+//     const userIdReg = /^[a-zA-Z0-9_.]/;
+//     const ans = userIdReg.test(test);
+//     console.log(ans);
+//     return ans;
+// }
+// 버튼 활성화
+// function btnChange() {
+//     if(newUserNames.value !== '' && focusrId.value !== '') {
+//         $saveBtn.disabled = false; 
+//     } else {
+//         $saveBtn.disabled = true;
+//     }
+// }
+
 // warning message remove
 const focusName = document.querySelector('#user-name');
-console.log(focusName);
+
 focusName.addEventListener("click", function () {
     warningMsg1.style.display = "none";
 });
 
-const focusId = document.querySelector('#user-id');
-focusId.addEventListener("click", function () {
+// const focusId = document.querySelector('#now-user-id');
+myId.addEventListener("click", function () {
     warningMsg2.style.display = "none";
 });
 
 //프로필 수정 값 체크 후 서버 전송
 async function editProfile() {
-    const userName = document.querySelector('#user-name').value;
-    const userId = document.querySelector('#user-id').value;
+    const newUserName = document.querySelector('#user-name').value;
     const intro = document.querySelector('#intro-input').value;
     const imageUrl = document.querySelector('.imgPre').src
-    console.log(imageUrl);
-
+    // const newUserId = myId.value;
+    if (userNameVal(newUserName)) {
+        return ;
+    }
     const res = await fetch(`${url}/user`, {
         method: "PUT",
         headers: {
@@ -119,29 +145,99 @@ async function editProfile() {
         },
         body: JSON.stringify({
             "user": {
-                "username": userName,
-                "accountname": userId,
+                "username": newUserName,
+                "accountname": myAccountName,
                 "intro": intro,
                 "image": imageUrl,
             }
         })
     })
-    console.log(res);
     const json = await res.json();
-
     if (json.status === 422) {
         warningMsg2.style.display = "block";
-    } else if (idError(false)) {
-        warningMsg1.style.display = "block";
-    }
-    // else if (json.status === 422 && idError(false)){
-    //     warningMsg2.style.display="block";
-    //     warningMsg1.style.display = "block";
-    // }
-    else {
+    // } else if (idError()) {
+    //     warningMsg3.style.display = "block";
+    } else {
         location.href = `/profile/${myAccountName}`
     }
-}
+    // if (checkIdValid(newUserId)) {
+    // }
 
-// 저장 버튼 
+}
+// // 저장 버튼 
 $saveBtn.addEventListener('click', editProfile);
+
+
+
+
+
+// else if (json.status === 422 && idError(false)){
+//     warningMsg2.style.display="block";
+//     warningMsg1.style.display = "block";
+// }
+// if (json.status === 422) {
+//     if(msg == '이미 사용중인 계정 ID입니다.') {
+//     warningMsg2.style.display = "block";
+//     } 
+// } else if (id_check(false)) {
+//     warningMsg1.style.display = "block";
+// } else (json.status === 200) {
+//     location.href = `/profile/${myAccountName}`;
+// }
+// }
+
+// if (id_check(true)) {
+//     if (json.status === 200) {
+//         location.href = `/profile/${myAccountName}`;
+//     } else {
+//         warningMsg2.style.display = "block";
+//     }
+// } else (id_check) {
+//     warningMsg1.style.display = "block";
+// }
+
+// if (json.status === 422) {
+
+// if (id_check(false)) {
+//     warningMsg1.style.display = "block";
+// } else ()
+// $saveBtn.classList.remove('disabled');
+//     location.href = `/profile/${myAccountName}`;
+// } else if(id_check(false)) {
+//     warningMsg1.style.display = "block";
+// } else if (json.status === 422) {
+//     warningMsg2.style.display = "block";
+// }
+
+// if (json.status === 422) {
+//     warningMsg2.style.display = "block";
+// } else if (id_check(false)) {
+//     warningMsg1.style.display = "block";
+// }
+// else if (json.status === 422 && idError(false)){
+//     warningMsg2.style.display="block";
+//     warningMsg1.style.display = "block";
+// }
+//     else (id_check(true) && json.status === 200) {
+//         location.href = `/profile/${myAccountName}`;
+//     }
+// }
+
+
+//         if (json.status === 422) {
+//             warningMsg2.style.display = "block";
+//         } else if (idError(false)) {
+//             warningMsg3.style.display = "block";
+//         }
+//         else {
+//             location.href = `/profile/${myAccountName}`
+// }
+
+// if (json.status === 200) {
+//     location.href = `/profile/${myAccountName}`
+// } else if (idError(false)) {
+//     warningMsg3.style.display = "block";
+// }
+// else {
+//     location.href = `/profile/${myAccountName}`
+// }
